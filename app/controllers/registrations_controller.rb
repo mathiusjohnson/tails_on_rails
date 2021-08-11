f6b39f1db5bd2d0952ad2256a159f5b947aba5ab
Mathius <devachievem@gmail.com>
2021-08-11 11:09:09 -0700

    query signup mutation with graphq-ruby execution

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,28 +3,89 @@
 class RegistrationsController < ApplicationController
 
 
-  SignupMutation = TailsOnRails::Client.query <<~GRAPHQL
-    mutation {
-      createUser(firstName: "'test'", authProvider: {
-          credentials: {
-            email: "'test@test.ca'", 
-            password: "'password'"
-          }
-        }
-      )
-    }
-  GRAPHQL
+
+  
 
   # POST /resource
   def create   
-    email = params[:session][:email]
-    password = params[:session][:password]
+    email = params[:session][:email].to_s
+    password = params[:session][:password].to_s
 
-    data = SignUpMutation
+    query_string = <<~GQL
+      mutation {
+        createUser(
+          firstName: "mathius", 
+          authProvider: {
+            credentials: {
+              email: "#{email}", 
+              password: "#{password}"
+            }
+          }
+        ) {
+          email
+          
+        }
+      }
+    GQL
+
+    # query_string = <<~GQL
+    #   mutation($first_name: string, $auth_provider: AuthProviderSignupData) {
+    #     createUser(
+    #       firstName: $first_name, 
+    #       authProvider: {
+    #         $auth_provider
+    #       }
+    #     ) {
+    #       email 
+    #     }
+    #   }
+    # GQL
+
+    # variables = {
+    # "firstName" => email,
+    # "authParams" => {
+    #   "credentials" => {
+    #     "email" => email,
+    #     "password" => password
+    #   }
+    # },
+    # }
+
+
+    response = GraphqlTutorialSchema.execute(query: query_string)
+
+    byebug
     respond_to do |format|
-      # format.json { render :json => response }
+      format.json { render :json => response }
+      format.html do
+        redirect_to '/'
+      end
     end
 
   end
 
 end
+
+
+# query_string = <<~GQL
+# mutation($first_name: string, $auth_provider: AuthProviderSignupData) {
+#   createUser(
+#     firstName: $first_name, 
+#     authProvider: {
+#       $auth_provider
+#     }
+#   ) {
+#     email 
+#   }
+# }
+# GQL
+
+# variables = {
+# "firstName" => email,
+# "authParams" => {
+#   "credentials" => {
+#     "email" => email,
+#     "password" => password
+#   }
+# },
+# }
