9b64d52b0f2939097d10ac69c4bbec58ca8aaa50
Mathius <devachievem@gmail.com>
2021-08-11 09:09:12 -0700

    refactor client connection

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,21 +1,21 @@
 class LinksController < ApplicationController
-  Client = Graphlient::Client.new('http://localhost:3000/graphql',
-    # headers: {
-    #   'Authorization' => 'Bearer 123'
-    # },
-    http_options: {
-      read_timeout: 20,
-      write_timeout: 30
-    }
-  )
+  # Client = Graphlient::Client.new('http://localhost:3000/graphql',
+  #   # headers: {
+  #   #   'Authorization' => 'Bearer 123'
+  #   # },
+  #   http_options: {
+  #     read_timeout: 20,
+  #     write_timeout: 30
+  #   }
+  # )
 
-  Links = Client.query <<~GRAPHQL
+  Links = TailsOnRails::Client.query <<~GRAPHQL
     query {
       allLinks {
         id
         description
         postedBy {
-          name
+          first_name
         }
         votes {
           user {
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,19 +1,11 @@
 # frozen_string_literal: true
 
-class RegistrationsController < Devise::RegistrationsController
-  Client = Graphlient::Client.new('http://localhost:3000/graphql',
-    # headers: {
-    #   'Authorization' => 'Bearer 123'
-    # },
-    http_options: {
-      read_timeout: 20,
-      write_timeout: 30
-    }
-  )
+class RegistrationsController < ApplicationController
 
-  Response = Client.query <<~GRAPHQL
+
+  SignupMutation = TailsOnRails::Client.query <<~GRAPHQL
     mutation {
-      createUser(name: "'test'", authProvider: {
+      createUser(firstName: "'test'", authProvider: {
           credentials: {
             email: "'test@test.ca'", 
             password: "'password'"
@@ -28,61 +20,11 @@ class RegistrationsController < Devise::RegistrationsController
     email = params[:session][:email]
     password = params[:session][:password]
 
-
-
-    puts Response.data
-
+    data = SignUpMutation
     respond_to do |format|
-      format.html {
-        redirect_to "/"
-      }
+      # format.json { render :json => response }
     end
 
   end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   params.require(:session).permit(:email, :password)
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
