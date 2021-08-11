a0e102a7e8d5ea17d23c72db20c12615d0a935fa
Mathius <devachievem@gmail.com>
2021-08-11 14:57:01 -0700

    refactor create user mutation

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -9,14 +9,30 @@ module Mutations
     argument :first_name, String, required: true
     argument :auth_provider, AuthProviderSignupData, required: false
 
+    field :token, String, null: true
+    field :user, Types::UserType, null: true
+
     type Types::UserType
 
     def resolve(first_name: nil, auth_provider: nil)
-      User.create!(
+      user = User.create!(
         first_name: first_name,
         email: auth_provider&.[](:credentials)&.[](:email),
         password: auth_provider&.[](:credentials)&.[](:password)
       )
+
+      return unless user
+
+      token = AuthToken.token_for_user(user)
+
+      context[:session][:token] = token
+
+      context[:current_user] = user[:email]
+
+      user
+
+    rescue ActiveRecord::RecordInvalid => e
+      GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
     end
   end
 end
